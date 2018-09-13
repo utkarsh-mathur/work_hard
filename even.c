e825cd6b042f6eabbe666fc2d67c285bd82cbb74
utkarsh-mathur <43219118+utkarsh-mathur@users.noreply.github.com>
2018-09-13 12:28:08 +0530

    complete

---
+++ even.c
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+ int n;
+ printf("Enter a number = ");
+ scanf("d",&n);
+ if (n%2==0)
+  printf("Even");
+ else
+  printf("odd")
+  getch();
+ }
